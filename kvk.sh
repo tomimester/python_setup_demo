b3c4aa1d1a3952cdeb2790ae212a14fa3e4d740c
Tomi Mester <mestitomi@gmail.com>
2026-07-01 23:04:45 +0200

    Enhance kvk.sh with SSH setup instructions
    Added instructions for SSH key generation and server configuration.

--- kvk.sh
+++ kvk.sh
@@ -3,13 +3,27 @@
 ### TELJES MAGYARÁZAT ITT: https://mestertomi.hu/
 ##########################
 
+######
+#LOCAL
+#szerverre bejelentkezés
+ssh root@[szerverIPcím]
+
+######
+#SERVER AS ROOT
 #új user hozzáadása
 adduser deploy
-
 #sudo jogok megadása
 usermod -aG sudo deploy
 
-#kilépés, belépés új userként (dataguy)
+#kijelentkezés
+exit
+
+######
+#LOCAL
+ssh deploy@[szerverIPcím]
+
+######
+#SERVER AS DEPLOY
 #megfelelő csomagok és kiegészítők telepítése
 sudo apt-get update
 sudo apt-get install mc
@@ -23,5 +37,30 @@ echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc && source ~/.bashrc
 claude --version
 claude
 
+########
+########
 #OPCIONÁLIS: erősebb security itt:
 # https://www.digitalocean.com/community/tutorials/how-to-configure-ssh-key-based-authentication-on-a-linux-server
+# PARANCSOK, DE NE KÖVESD VAKON!!!
+######
+#LOCAL
+ssh-keygen #kulcsgenerálás, CSAK HA NINCS!!
+ssh-copy-id deploy@[IPcím]
+ssh deploy@[szerverIPcím]
+
+############
+#SERVER AS DEPLOY
+sudo mcedit /etc/ssh/sshd_config
+
+### a fájlba ezeket állítsd be
+PasswordAuthentication no
+PermitRootLogin no
+PubkeyAuthentication yes
+### SAVE & EXIT
+
+sudo systemctl restart ssh
+
+
+
+
+
